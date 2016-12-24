876ffb0398bf28c0a02717a48f2165b123336d9b
Lucas Ron <ronlucas86@gmail.com>
2016-12-24 14:04:20 -0300

    estos cambios se tendrian que haber hecho en un branch

---
+++ src/main/java/com/rulo/app/model/User.java
@@ -0,0 +1,50 @@
+package com.rulo.app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USUARIO", schema="PRUEBA")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+
+	private String fullName;
+
+	private String userName;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
